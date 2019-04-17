96e7ac1b1600c2e1486039bb832c99b575659916
ddidwyll <ddidwyll@gmail.com>
2019-04-17 12:45:39 +0400

    mcpdq93jfpoawkf3

--- lib/tree.ex
+++ lib/tree.ex
@@ -9,7 +9,7 @@ defmodule Tree do
 
   @struct [:id_br_st, :gid, :uid, :upd, :json]
 
-  def init() do
+  def init do
     table(:tree, :set, @struct)
     table(:bag, :bag, @struct)
   end
@@ -30,14 +30,14 @@ defmodule Tree do
   end
 end
 
+#
+#
+#
+#
+#
+
 defmodule Tree.Store do
   @moduledoc false
-
-  #
-  #
-  #
-  #
-  #
 
   import :mnesia,
     only: [
@@ -77,8 +77,8 @@ defmodule Tree.Store do
     |> post(rec)
   end
 
-  defp post(tuple, rec) do
-    now = now()
+  defp post(tuple, rec, time \\ nil) do
+    now = (time || now()) |> to_string
     json = encode!(rec, @html)
 
     {:atomic, result} =
@@ -188,21 +188,19 @@ defmodule Tree.Store do
     do: get_all(db, branch, status, gid, :_)
 end
 
-defmodule Tree.Do do
+#
+#
+#
+#
+#
+
+defmodule Tree.Main do
   @moduledoc false
 
-  #
-  #
-  #
-  #
-  #
   import Tree.Store
   import Tree.Guards
   import Tree.Validator
   import String, only: [capitalize: 1]
-
-  # import :mnesia,
-  #   only: [transaction: 1]
 
   import :cowboy_req,
     only: [read_body: 1, set_resp_body: 2]
@@ -261,7 +259,7 @@ defmodule Tree.Do do
           state.in
         )
 
-      message = title(state) <> "was posted"
+      message = title(state) <> "posted successful"
       {:ok, req, %{state | from: time, to: id, out: message}}
     else
       {result, req, state}
@@ -298,22 +296,24 @@ defmodule Tree.Do do
         %{state | gid: gid, uid: uid, upd: upd, out: json}
       }
     else
-      0 -> {false, req, state}
       _ -> {false, req, state}
     end
   end
 
   def status(state) do
     statuses =
-      get_all(
-        state.schema["type"],
-        state.branch,
-        :_,
-        :_,
-        :_,
-        state.from
-      )
-      |> list_status
+      if state.from,
+        do:
+          get_all(
+            state.schema["type"],
+            state.branch,
+            :_,
+            :_,
+            :_,
+            state.from
+          )
+          |> list_status,
+        else: []
 
     cond do
       is_empty_list(statuses) ->
@@ -326,7 +326,7 @@ defmodule Tree.Do do
         %{state | out: title(state) <> "blocked"}
 
       statuses == [:archived] || :archived in [statuses] ->
-        to = "/archive/#{state.branch}/#{state.from}"
+        to = {true, "/archive/#{state.branch}/#{state.from}"}
         %{state | out: title(state) <> "moved to archive", to: to}
     end
   end
@@ -341,14 +341,14 @@ defmodule Tree.Do do
   end
 end
 
+#
+#
+#
+#
+#
+
 defmodule Tree.Route do
   @moduledoc false
-
-  #
-  #
-  #
-  #
-  #
 
   @behaviour :cowboy_rest
 
@@ -361,9 +361,10 @@ defmodule Tree.Route do
   import :erlang,
     only: [posixtime_to_universaltime: 1]
 
+  import String, only: [slice: 3, to_integer: 1]
   import Tree.Config, only: [schema: 1]
   import Logger, only: [info: 1]
-  import Tree.Do
+  import Tree.Main
 
   @methods [
     "OPTIONS",
@@ -404,7 +405,6 @@ defmodule Tree.Route do
       from: req0.bindings[:from],
       type: req0.bindings.type,
       to: req0.bindings[:to],
-      method: req0.method,
       uid: "ddidwyll",
       gid: "work",
       upd: nil,
@@ -418,43 +418,49 @@ defmodule Tree.Route do
 
   @impl true
   def content_types_provided(req, state) do
-    info("content_types_provided")
+    # info("content_types_provided")
     {[@to_json], req, state}
   end
 
   @impl true
   def content_types_accepted(req, state) do
-    info("content_types_accepted")
+    # info("content_types_accepted")
     {[@from_json], req, state}
   end
 
   @impl true
   def allowed_methods(req, state) do
-    info("allowed_methods")
+    # info("allowed_methods")
     {@methods, req, state}
   end
 
   @impl true
   def charsets_provided(req, state) do
-    info("charsets_provided")
+    # info("charsets_provided")
     {[@utf8], req, state}
   end
 
   @impl true
   def delete_completed(req, state) do
-    info("delete_completed")
+    # info("delete_completed")
+    {false, req, state}
+  end
+
+  @impl true
+  def delete_resource(req, state) do
+    # info("delete_resource")
     {false, req, state}
   end
 
   @impl true
   def resource_exists(req, state) do
-    info("resource_exists")
+    # info("resource_exists")
     exist(req, state)
   end
 
   @impl true
   def previously_existed(req, state0) do
-    info("previously_existed")
+    # info("previously_existed")
     state = status(state0)
 
     {!!state.out, req, state}
@@ -463,35 +469,24 @@ defmodule Tree.Route do
 
   @impl true
   def moved_permanently(req, state) do
-    info("moved_permanently")
-
-    moved =
-      if state.to,
-        do: {true, state.to},
-        else: false
-
-    {moved, req, state}
-  end
-
-  @impl true
-  def delete_resource(req, state) do
-    info("delete_resource")
-    {false, req, state}
+    # info("moved_permanently")
+    {state.to || false, req, state}
   end
 
   @impl true
   def is_authorized(req, state) do
-    info("is_authorized")
+    # info("is_authorized")
     {true, req, state}
   end
 
   @impl true
   def last_modified(req, state) do
-    info("last_modified")
+    # info("last_modified")
 
     last_mod =
       state.upd
-      |> div(1000)
+      |> slice(0, 10)
+      |> to_integer()
       |> posixtime_to_universaltime()
 
     {last_mod, req, state}
@@ -499,14 +494,14 @@ defmodule Tree.Route do
 
   @impl true
   def forbidden(req, state) do
-    info("forbidden")
+    # info("forbidden")
     {false, req, state}
   end
 
   @impl true
   def malformed_request(req, state) do
     cond do
-      state.type not in ["tree", "archive"] ->
+      state.type not in ["rest", "archive"] ->
         message = "\"Wrong type #{state.type}\""
         {true, set_resp_body(message, req), state}
 
@@ -524,7 +519,7 @@ defmodule Tree.Route do
   end
 
   def from_json(req, state) do
-    case state.method do
+    case req.method do
       "POST" -> post(req, state)
     end
   end
